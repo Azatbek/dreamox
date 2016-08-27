a20dc946385e3c8d2c7540e92275f1a49c7bf98c
Tumenbayev <adilbek.tumenbayev@gmail.com>
2016-08-27 19:02:02 +0600

    2nd commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,30 @@
 @extends('app')
 
 @section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
-
-				<div class="panel-body">
-					You are logged in!
+<div class="intro">
+		<div class="vertical-align">
+			<div class="transparent-text">
+				<h1>Выбирай лучшее с Your Dream Box!</h1>
+				<div class="flex-list">
+				  <ul>
+					<li>Лучшие коробки</li>
+					<li>Дешево</li>
+					<li>Бесплатная доставка</li>
+				  </ul>
 				</div>
+				<a href="#shop" class="btn btn-primary btn-lg sharp">Что дальше?</a>
 			</div>
 		</div>
 	</div>
-</div>
+    <!-- Page Content -->
+    <div class="container" id="shop">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="row">
+                  @include('front.part.home')
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.container -->
 @endsection
